bbfe1a15bd05a968af45f35af55b0d9da80b932f
sallespro <sallespro@gmail.com>
2026-02-09 10:49:12 -0300

    regex routing

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
 
     // Handle client-side routing
-    app.get('(.*)', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
 }
